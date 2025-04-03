bdcf3fef38ca27f6aec588c280e99cc29139d3cc
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-04-03 09:27:38 -0400

    add basic SVG Text class. No rendering support yet.

--- include/Bount/SVG/Text.hpp
+++ include/Bount/SVG/Text.hpp
@@ -3,15 +3,41 @@
 
 #include "Bount/SVG/Lib-Macro.hpp"
 #include "Bount/SVG/Node.hpp"
+#include "Bount/SVG/Helpers/File.hpp"
+#include <svgpp/svgpp.hpp>
 
 namespace Bount::SVG
 {
-class Text : public Element
+class Text final : public Drawable
 {
+    String _text;
+    
 public:
-    BOUNT_SVG_API Text();
+    BOUNT_SVG_API Text(File file);
     BOUNT_SVG_API ~Text();
+
     BOUNT_SVG_API Type getElementType() const override;
+    BOUNT_SVG_API void updateUniforms() override;
+
+    template <class Range>
+    BOUNT_SVG_API void set(svgpp::tag::attribute::x, const Range& value);
+    template <class Range>
+    BOUNT_SVG_API void set(svgpp::tag::attribute::y, const Range& value);
+    template <class Range>
+    BOUNT_SVG_API void set(svgpp::tag::attribute::dx, const Range& value);
+    template <class Range>
+    BOUNT_SVG_API void set(svgpp::tag::attribute::dy, const Range& value);
+
+    template <class Range>
+    BOUNT_SVG_API void set(svgpp::tag::attribute::rotate, const Range& value);
+
+    template <class Range>
+    BOUNT_SVG_API void set(svgpp::tag::attribute::textLength, const Range& value);
+    template <class Range>
+    BOUNT_SVG_API void set(svgpp::tag::attribute::lengthAdjust, const Range& value);
+
+    template <class Range>
+    BOUNT_SVG_API void set_text(const Range& text);
 };
 }
 
